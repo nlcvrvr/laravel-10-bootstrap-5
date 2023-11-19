fb37dc39e5d94ffb42d04bf52eba907270ddc023
nlcvrvr <48892488+nlcvrvr@users.noreply.github.com>
2023-11-19 11:49:53 +0000

    Vite HMR OK, But Icons not showing

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'friendly-fiesta-9jxp6j4p759cjg5-5173.app.github.dev',
+            protocol: 'wss',
+            clientPort: 443,
+        }
+    },
     plugins: [
         laravel({
             input: [
